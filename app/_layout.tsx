1822f218974463e0c9a993ce0220ee60615e7030
Daniel Craciun <danielcraciun111@yahoo.com>
2025-07-22 16:50:34 +0100

    refactor: made root layout be empty with sub layout handling the sidebar and tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,13 @@
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
-import { Drawer } from "expo-router/drawer";
-import * as SplashScreen from "expo-splash-screen";
-import { useColorScheme } from "nativewind";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Slot, SplashScreen } from "expo-router";
 
 import "../global.css";
-
-import Ionicons from "@expo/vector-icons/build/Ionicons";
-
-SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
-  const { colorScheme } = useColorScheme();
 
   useEffect(() => {
     if (loaded) {
@@ -27,59 +19,5 @@ export default function Layout() {
     return null;
   }
 
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer
-        screenOptions={{
-          headerShown: true,
-          headerTitle: "",
-          drawerPosition: "left",
-          headerStyle: {
-            backgroundColor:
-              colorScheme === "dark" ? "#000000" : "#FFFFFF",
-          },
-          headerTintColor: colorScheme === "dark" ? "#FFFFFF" : "#023c69",
-          drawerActiveTintColor:
-            colorScheme === "dark" ? "#FFFFFF" : "#023c69",
-          drawerInactiveTintColor:
-            colorScheme === "dark" ? "#9CA3AF" : "#6B7280",
-          drawerStyle: {
-            backgroundColor:
-              colorScheme === "dark" ? "#000000" : "#FFFFFF",
-          },
-        }}
-      >
-        <Drawer.Screen
-          name="(tabs)"
-          options={{
-            title: "Home",
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name="home-outline" size={size} color={color} />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="settings"
-          options={{
-            drawerLabel: "Settings",
-            drawerIcon: ({ color, size }) => (
-              <Ionicons
-                name="settings-outline"
-                size={size}
-                color={color}
-              />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="play/index"
-          options={{
-            drawerItemStyle: {
-              display: "none",
-            },
-          }}
-        />
-      </Drawer>
-    </GestureHandlerRootView>
-  );
+  return <Slot />;
 }
